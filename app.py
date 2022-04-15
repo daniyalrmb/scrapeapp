ce4b24b0ddaff863e657a80d4d5f664aa046fd3f
daniyalrmb <77576726+daniyalrmb@users.noreply.github.com>
2022-04-15 19:40:52 +0500

    Update app.py

--- app.py
+++ app.py
@@ -13,10 +13,8 @@ def getData(search):
     p=[]
     URL = 'https://www.daraz.pk/catalog/?q={}&_keyori=ss&from=input&spm=a2a0e.home.search.go.35e34937MnH6tM'.format(search)
     html_doc = requests.get(URL).text
-    data = json.loads(re.search(r'window\.pageData=({.*})', html_doc).group(1))
-    for item in data['mods']['listItems']:
-        p.append(item['name'])
-    return p
+
+    return html_doc
     
 st.title("Daraz Scraper")
 search = st.text_input('Enter search term')
