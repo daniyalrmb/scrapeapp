d8de249036e7d04abd8f3a5d4c4de2af8b59b456
daniyalrmb <77576726+daniyalrmb@users.noreply.github.com>
2022-04-16 03:00:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ import urllib3
 def getData(search):
 
   
-    ua = UserAgent()
     headers = {'User-Agent':'Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11'}
     
     URL = 'https://www.daraz.pk/catalog/?q={}&_keyori=ss&from=input&spm=a2a0e.home.search.go.35e34937MnH6tM'.format(search)
