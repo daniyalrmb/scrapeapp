28aa0280874b27b242f20cf31784358b6e960e0b
daniyalrmb <77576726+daniyalrmb@users.noreply.github.com>
2022-04-16 02:35:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,20 +11,10 @@ import urllib3
 # accessing Chromedriver
 def getData(search):
 
-    
-    
-    headers = { 'accept':'*/*',
-    'accept-encoding':'gzip, deflate, br',
-    'accept-language':'en-GB,en;q=0.9,en-US;q=0.8,hi;q=0.7,la;q=0.6',
-    'cache-control':'no-cache',
-    'dnt':'1',
-    'pragma':'no-cache',
-    'referer':'https',
-    'sec-fetch-mode':'no-cors',
-    'sec-fetch-site':'cross-site',
-    'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
     }
-
+    
     URL = 'https://www.daraz.pk/catalog/?q={}&_keyori=ss&from=input&spm=a2a0e.home.search.go.35e34937MnH6tM'.format(search)
 
     html_doc = requests.get(url = URL, headers = headers)
