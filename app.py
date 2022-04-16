8907f7f5b93a44f65c56ff77d91aefd062328045
daniyalrmb <77576726+daniyalrmb@users.noreply.github.com>
2022-04-16 08:08:13 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,60 +3,13 @@ import streamlit as st
 import time
 import urllib3
 import requests
-import json
 import re
-import os
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium_stealth import stealth
-from fake_useragent import UserAgent
-from webdriver_manager.chrome import ChromeDriverManager
 
 def getData(search):
-    listt = []
-    t0 = time.time()
-
-
-    chrome_options = webdriver.ChromeOptions()
-    ua = UserAgent()
-    userAgent = ua.random
-    chrome_options.add_argument(f'user-agent={userAgent}')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--proxy-sever=socks5://127.0.0.1:0000')
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-    chrome_options.add_argument("window-size=1200x600")
-    chrome_options.add_argument("--window-position=0,0")
-    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    chrome_options.add_experimental_option('useAutomationExtension', False)
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
- 
-    driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
-    
-    stealth(driver,
-        languages=["en-US", "en"],
-        vendor="Google Inc.",
-        platform="Win32",
-        webgl_vendor="Intel Inc.",
-        renderer="Intel Iris OpenGL Engine",
-        fix_hairline=True,
-        )
-
 
     start_url = 'https://www.olx.com.pk/items/q-{}'.format(text)
-    driver.get(start_url)
-    time.sleep(2)
-    source = driver.page_source
+    source = requests.get(start_url).text
     data = re.findall('" aria-label="Title">(.*)</div><div class="_52497c97"', source)
-    #listt.append(data)
-
-    # b'<!DOCTYPE html><html xmlns="http://www....
-    driver.quit()
-    t1 = time.time()
-    total = t1-t0
     
     df = pd.DataFrame(data)
     return df
