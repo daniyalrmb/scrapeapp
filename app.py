503c2f766a76a5a2b053a134cb8a0d491e5232dd
daniyalrmb <77576726+daniyalrmb@users.noreply.github.com>
2022-04-16 10:07:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import urllib3
 import requests
 import re
 
-def getData(search):
+def getData(search, pages):
 
     #start_url = 'https://www.olx.com.pk/items/q-{}'.format(search)
     start_url = 'https://www.aliexpress.com/wholesale?catId=0&initiative_id=SB_20220416075355&SearchText={}&spm=a2g0o.home.1000002.0'.format(search)
@@ -13,13 +13,25 @@ def getData(search):
     data = re.findall('"storeName":"(.*?)","storeId":', str(source))
     #source = requests.get(start_url).text
     #data = re.findall('" aria-label="Title">(.*)</div><div class="_52497c97"', source)
-    return data
+    dat = []
+    
+    for i in range(1, pages):
+        sec_url = 'https://www.aliexpress.com/wholesale?trafficChannel=main&d=y&CatId=0&SearchText=lamp&ltype=wholesale&SortType=default&page={}'.format(i)
+        source = requests.get(sec_url).content
+        dat.append(re.findall('"storeName":"(.*?)","storeId":', str(source)))
+
+    flat_list = [item for sublist in dat for item in sublist]
+    data = data + flat_list
+    df = pd.DataFrame(data, columns =['Stores'])
+    
+    return df
     
 st.title("olx.pk Scraper")
 search = st.text_input('Enter search term')
+pages = st.number_input('Enter number of pages here')
 
 time.sleep(5)
-p = getData(search)
+p = getData(search, pages)
 time.sleep(5)
-st.subheader(p)
+st.dataframe(p)
 st.subheader(" Recent Repositories")
