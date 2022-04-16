a3e0e7bcf2c8a5c9aacdeecdac3ccd2c24200be1
daniyalrmb <77576726+daniyalrmb@users.noreply.github.com>
2022-04-16 09:20:40 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,12 @@ import re
 
 def getData(search):
 
-    start_url = 'https://www.olx.com.pk/items/q-{}'.format(search)
-    source = requests.get(start_url).text
-    data = re.findall('" aria-label="Title">(.*)</div><div class="_52497c97"', source)
+    #start_url = 'https://www.olx.com.pk/items/q-{}'.format(search)
+    start_url = 'https://www.aliexpress.com/wholesale?catId=0&initiative_id=SB_20220416075355&SearchText={}&spm=a2g0o.home.1000002.0'.format(search)
+    source = requests.get(start_url).content
+    data = re.findall('"storeName":"(.*?)","storeId":', str(source))
+    #source = requests.get(start_url).text
+    #data = re.findall('" aria-label="Title">(.*)</div><div class="_52497c97"', source)
     return data
     
 st.title("olx.pk Scraper")
